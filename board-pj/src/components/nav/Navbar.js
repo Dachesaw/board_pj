dfb9ddd2ef042fd5f45ab19e4bec56f5dd75c454
Dachesaw <123497362+Dachesaw@users.noreply.github.com>
2024-04-18 14:05:23 +0300

    add nav bar and style

--- board-pj/src/components/nav/Navbar.js
+++ board-pj/src/components/nav/Navbar.js
@@ -1,21 +1,24 @@
-import './nav.css';
+import '../../css/assets/nav.css';
 
 import loc from '../../img/svg/loc.svg';
-import menus from '../../img/svg/image.png';
+import menus from '../../img/svg/menus.svg';
+import pay from '../../img/svg/pay-reciving.svg';
+import refund from '../../img/svg/refund.svg';
+import delivery from '../../img/svg/delivery.svg';
 
 const Navbar = () => {
     return ( 
         <nav>
             <ul>
-                <li><img src={loc}/><a><span>Регион:</span><span>Москва</span><img src={menus}/></a></li>
-                <li><a>Магазины</a></li>
-                <li><a>Помощь</a></li>
-                <li><a>Блоги</a></li>
+                <li><a href=''><img  src={loc}/><span className='gap'>Регион:</span><span>Москва</span><img src={menus}/></a></li>
+                <li><a href=''><span>Магазины</span></a></li>
+                <li><a href=''><span>Помощь</span></a></li>
+                <li><a href=''><span>Блоги</span></a></li>
             </ul>
             <ul>
-                <li><a>Бесплатная доставка *</a></li>
-                <li><a>Оплата при получении</a></li>
-                <li><a>Возврат в течение 14 дней</a></li>
+                <li><a href=''><img src={delivery}/><span> Бесплатная доставка *</span></a></li>
+                <li><a href=''><img src={pay}/><span>Оплата при получении</span></a></li>
+                <li><a href=''><img src={refund}/><span>Возврат в течение 14 дней</span></a></li>
             </ul>
         </nav>
      );
